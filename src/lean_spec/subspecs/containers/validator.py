cec963d8fe04e8ce1acf51776e5e008d9116d6b6
Thomas Coratger <thomas.coratger@gmail.com>
2025-12-31 12:50:58 +0100

    validator: rm useless produce_attestation

--- src/lean_spec/subspecs/containers/validator.py
+++ src/lean_spec/subspecs/containers/validator.py
@@ -5,7 +5,6 @@ from __future__ import annotations
 from lean_spec.types import Bytes52, Container, Uint64
 
 from ..xmss.containers import PublicKey
-from .attestation import Attestation, AttestationData
 
 
 class Validator(Container):
@@ -20,21 +19,3 @@ class Validator(Container):
     def get_pubkey(self) -> PublicKey:
         """Get the XMSS public key from this validator."""
         return PublicKey.decode_bytes(bytes(self.pubkey))
-
-    def produce_attestation(self, data: AttestationData) -> Attestation:
-        """
-        Produce an attestation from attestation data.
-
-        This method wraps AttestationData with the validator's identity to create
-        a complete Attestation object ready for signing and broadcast.
-
-        Args:
-            data: The attestation data containing slot, head, target, and source.
-
-        Returns:
-            A fully constructed Attestation object with this validator's index.
-        """
-        return Attestation(
-            validator_id=self.index,
-            data=data,
-        )
