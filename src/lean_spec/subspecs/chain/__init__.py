5bc9ef36d17e6519eaa18666d5843c51f6acf2ac
Thomas Coratger <thomas.coratger@gmail.com>
2026-01-07 23:38:18 +0100

    chain: add wall-clock time to consensus slots converter

--- src/lean_spec/subspecs/chain/__init__.py
+++ src/lean_spec/subspecs/chain/__init__.py
@@ -1,7 +1,10 @@
 """Specifications for chain and consensus parameters."""
 
+from .clock import Interval, SlotClock
 from .config import DEVNET_CONFIG
 
 __all__ = [
     "DEVNET_CONFIG",
+    "Interval",
+    "SlotClock",
 ]
